1e8ebefb94f327714e1c19c5d68801a20877d713
Jorge Ruizcaro <jorge_ruizcaro@questsys.com>
2023-03-08 18:04:02 -0500

    new project

---
+++ src/webparts/webpart/WebpartWebPart.ts
@@ -0,0 +1,170 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+  PropertyPaneCheckbox,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+
+import {
+
+  HttpClient,
+  HttpClientResponse
+} from '@microsoft/sp-http';
+import styles from './WebpartWebPart.module.scss';
+import * as strings from 'WebpartWebPartStrings';
+
+export interface IWebpartWebPartProps {
+  description: string;
+  test: string;
+  test1: boolean;
+  test2: string;
+  test3: boolean;
+}
+
+export interface ISPLists {
+  value: ISPList[];
+}
+
+export interface ISPList {
+  capital: string;
+  region: string;
+  subregion: string;
+}
+
+export default class WebpartWebPart extends BaseClientSideWebPart<IWebpartWebPartProps> {
+
+  public render(): void {
+   
+  let html: string = '';
+  this.context.httpClient.get('https://restcountries.com/v3.1/region/europe', HttpClient.configurations.v1)
+  .then((res: HttpClientResponse): Promise<any> => {
+    return res.json();
+  })
+  .then((response: any): void => {
+    //console.log(response);
+    
+    response.forEach((item: any) => {
+      html += `
+      <section class="${styles.webpart} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+          <ul class="${styles.list}">
+          <li class="${styles.listItem}">
+            <span class="ms-font-l">${item.capital}</span>
+          </li>
+        </ul>
+      </section>`;
+    //console.log(html); 
+    });
+  
+    this.domElement.innerHTML=html;
+    
+  });
+
+  //if you want more information about callin apis https://learn.microsoft.com/en-us/sharepoint/dev/spfx/connect-to-anonymous-apis
+
+  
+}
+
+  protected onInit(): Promise<void> {
+    return this._getEnvironmentMessage().then(message => {
+      //this._environmentMessage = message;
+    });
+  }
+
+  
+
+  private _getEnvironmentMessage(): Promise<string> {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+        .then(context => {
+          let environmentMessage: string = '';
+          switch (context.app.host.name) {
+            case 'Office': // running in Office
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+              break;
+            case 'Outlook': // running in Outlook
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+              break;
+            case 'Teams': // running in Teams
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+              break;
+            default:
+              throw new Error('Unknown host');
+          }
+
+          return environmentMessage;
+        });
+    }
+
+    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    //this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+              PropertyPaneTextField('description', {
+                label: 'Description'
+              }),
+              PropertyPaneTextField('test', {
+                label: 'Multi-line Text Field',
+                multiline: true
+              }),
+              PropertyPaneCheckbox('test1', {
+                text: 'Checkbox'
+              }),
+              PropertyPaneDropdown('test2', {
+                label: 'Dropdown',
+                options: [
+                  { key: '1', text: 'One' },
+                  { key: '2', text: 'Two' },
+                  { key: '3', text: 'Three' },
+                  { key: '4', text: 'Four' }
+  
+  
+                ]}),
+              PropertyPaneToggle('test3', {
+                label: 'Toggle',
+                onText: 'On',
+                offText: 'Off'
+              })
+            ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/webpart/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'WebpartWebPartStrings' {
+  const strings: IWebpartWebPartStrings;
+  export = strings;
+}
